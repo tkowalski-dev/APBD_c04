fbe74150ba9169c6058338ba0585084600477591
tkowalski-dev <81387365+tkowalski-dev@users.noreply.github.com>
2024-04-28 13:01:15 +0200

    Init

---
+++ APBD_c04/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!");
+
+using System.Data.SqlClient;
+
+SqlTransaction tran = null;
+
+try
+{
+    SqlConnection con = new SqlConnection("");
+    tran = con.BeginTransaction();
+
+    SqlCommand comm = new SqlCommand("");
+    comm.Connection = con;
+    comm.Transaction = tran;
+    
+    tran.Commit();
+    // tran.CommitAsync();
+}
+catch (Exception ex)
+{
+    // ignored
+    tran?.Rollback();
+}
